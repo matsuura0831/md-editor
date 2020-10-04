be727917f0f9b046d0866f08393a24d343c3e09d
matsuura0831 <matsuura0831@gmail.com>
2020-10-04 23:39:41 +0900

    update.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,12 +12,19 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/ja';
 dayjs.locale('ja');
 
+import frontmatter from '@github-docs/frontmatter';
+
+import { DIR_NOTEBOOK, DIR_WORKDIR, readFileRecursively, readFile } from './js/util-file';
+import { db_init, db_insert, db_update, db_find } from './js/util-db';
 import { enable_viewer, enable_drawio} from './js/util-ace';
 import './base.css'
 
 const PLANTUML_SERVER = 'http://www.plantuml.com/plantuml'
 const DIAGRAMS_SERVER = 'https://embed.diagrams.net/?embed=1&ui=min&spin=1&proto=json&configure=1';
 
+/*
+    マークダウンパーサの準備
+*/
 const md = require('markdown-it')({
         linkify: true,
         breaks: true,
@@ -34,17 +41,21 @@ const md = require('markdown-it')({
     .use(require('markdown-it-sup'))
     .use(require('markdown-it-footnote'))
     .use(require('markdown-it-emoji'))
+    // load custom plugins
     .use(require('./js/markdown-it-drawio'))
     .use(require('./js/markdown-it-highlight'))
     .use(require('./js/markdown-it-message'))
     .use(require('./js/markdown-it-inject-line-no'));
 
+/*
+    DOMの取得及びAceEditorの設定
+*/
 const nav_notebook = document.querySelector('#nav-notebook');
 const nav_page = document.querySelector('#nav-page');
 const viewer = document.querySelector('#viewer');
 const _editor = document.querySelector('#editor');
 
-const editor = ace.edit('editor',  {
+const editor = ace.edit('editor', {
     theme: 'ace/theme/monokai',
     mode: 'ace/mode/markdown',
     wrap: true,
@@ -53,7 +64,7 @@ const editor = ace.edit('editor',  {
 });
 editor.setKeyboardHandler("ace/keyboard/vim");
 
-// sample
+// キーバインディングは以下のように取得できる
 ace.config.loadModule("ace/keyboard/vim", function(m) {
     var VimApi = m.CodeMirror.Vim
     VimApi.defineEx("write", "w", function(cm, input) {
@@ -61,6 +72,9 @@ ace.config.loadModule("ace/keyboard/vim", function(m) {
     })
 })
 
+/*
+    toggleから始まるボタンを押下した際に対になるパネルの表示・非表示を切り替える
+*/
 const tg_notebook = document.querySelector('#toggle-notebook');
 const tg_page = document.querySelector('#toggle-page');
 const tg_editor = document.querySelector('#toggle-editor');
@@ -82,6 +96,11 @@ mapping.forEach(i => {
     })
 });
 
+/*
+    ツールボタンの設定
+    invoke-drawio: drawio画面を開く．現在カーソル位置に応じて新規作成と編集を切り替える
+    invoke-now: 現在時刻を現在カーソル位置に挿入する
+*/
 const bt_drawio = document.querySelector('#invoke-drawio');
 const bt_time = document.querySelector('#invoke-now')
 
@@ -93,6 +112,144 @@ enable_drawio(editor, bt_drawio, DIAGRAMS_SERVER);
 bt_time.addEventListener('click', () => {
     const t = `${dayjs().format()}\n`;
     editor.session.insert(editor.getCursorPosition(), t);
+});
+
+
+/*
+    ローカルのマークダウンのロード
+*/
+const list_notebook = document.querySelector('#list-notebook');
+const list_tag = document.querySelector('#list-tag');
+const list_page = document.querySelector('#list-page');
+
+function init_lists() {
+    list_notebook.querySelectorAll('li').forEach((elm) => {
+        elm.classList.remove('highlight-notebook')
+    })
+    list_tag.querySelectorAll('li').forEach((elm) => {
+        elm.classList.remove('highlight-tag')
+    })
+}
+function init_pages() {
+    list_page.querySelectorAll('li').forEach((elm) => {
+        elm.classList.remove('highlight-page');
+    });
+}
+
+function update_pages(docs) {
+    list_page.innerHTML = ""
+
+    docs.forEach((d) => {
+        const li = document.createElement('li');
+        li.classList.add('p-1', 'truncate');
+        li.innerHTML = `<span class="mr-1"><i class="far fa-file"></i></span> ${d.title}</li>`;
+        li.setAttribute('data-path', d.path);
+
+        list_page.appendChild(li);
+    });
+
+    list_page.querySelectorAll('li').forEach((elm) => {
+        elm.addEventListener('click', async function() {
+            init_pages();
+            this.classList.add('highlight-page');
+
+            const fp = this.getAttribute('data-path');
+            const data = await readFile(fp, 'utf-8');
+
+            console.log(fp, data);
+            editor.setValue(data, 1);
+        })
+    });
+}
+
+db_init('markdown', [DIR_WORKDIR, 'user.neodb']);
+
+readFileRecursively(DIR_NOTEBOOK).then(async (files) => {
+    const messages = await Promise.all(files.map((f) => {
+        return new Promise(async (resolve, reject) => {
+            const markdown = await readFile(f.path, "utf-8");
+
+            const { data, errors } = frontmatter(markdown);
+            if (errors.length > 0) {
+                reject(errors);
+            } else {
+                resolve([f, data]);
+            }
+        });
+    }));
+
+    const updates = messages.map(m => {
+        const [fp, fm] = m;
+        const query = { path: fp.path };
+        const opt = { upsert: true }
+
+        const create_at = fm.create_at ? dayjs(fm.create_at) : dayjs();
+        const update_at = fm.update_at ? dayjs(fm.update_at) : dayjs();
+
+        return db_update('markdown', query, {
+            path: fp.path,
+            notebook: fp.notebook,
+            title: fm.title || fp.file.split('.')[0],
+            create_at: create_at.format(),
+            update_at: update_at.format(),
+            tags: fm.tags || [],
+        }, opt);
+    });
+
+    Promise.all(updates).then(() => {
+        db_find('markdown', {}, {notebook:1}).then((docs) => {
+            list_notebook.innerHTML = "";
+
+            const books = docs.map((d) => d.notebook);
+
+            [...new Set(books)].forEach((b) =>{
+                const li = document.createElement('li');
+                li.classList.add('data-notebook', 'p-1');
+                li.innerText = b;
+                li.setAttribute('data-notebook', b);
+
+                list_notebook.appendChild(li);
+            });
+
+            list_notebook.querySelectorAll('li').forEach((elm) => {
+                elm.addEventListener('click', function(ev) {
+                    init_lists();
+                    this.classList.add('highlight-notebook');
+
+                    const notebook = this.getAttribute('data-notebook');
+                    db_find('markdown', {notebook: notebook}, {path:1, title:1}).then(update_pages);
+                });
+            })
+        });
+
+        db_find('markdown', {tags: { $elemMatch: "Android"}}, {tags:1}).then((docs) => {
+            list_tag.innerHTML = ""
+
+            let tags = [];
+            docs.forEach((d) => {
+                tags = [...tags, ...d.tags]
+            });
+
+            [...new Set(tags)].forEach((t) => {
+                const li = document.createElement('li');
+                li.classList.add('data-tag', 'p-1');
+                li.innerText = `# ${t}`;
+                li.setAttribute('data-tag', t);
+                
+                list_tag.appendChild(li)
+            })
+
+            list_tag.querySelectorAll('li').forEach((elm) => {
+                elm.addEventListener('click', function(ev) {
+                    init_lists();
+                    this.classList.add('highlight-tag');
+
+                    const tag = this.getAttribute('data-tag');
+                    db_find('markdown', {tags: { $elemMatch: tag }}, {path:1, title:1}).then(update_pages);
+                });
+            })
+        });
+    })
 });
 
 
