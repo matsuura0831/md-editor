46dd67166bc32bcf47a74e540f25bd037433add0
matsuura0831 <matsuura0831@gmail.com>
2020-10-04 23:39:31 +0900

    update.

--- frontend/src/js/util-file.js
+++ frontend/src/js/util-file.js
@@ -1,20 +1,48 @@
-import fs from 'fs';
 import path from 'path';
+
+import fs from 'fs';
+const fsPromises = fs.promises;
 
 import { remote } from 'electron';
 const { process } = remote;
 
-var dir_home = process.env['MD_HOME'] || process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
-var dir_workdir = path.join(dir_home, '.mde');
-var dir_notebook = path.join(dir_workdir, 'notebook');
+const DIR_HOME = process.env['MD_HOME'] || process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+const DIR_WORKDIR = path.join(DIR_HOME, '.mde');
+const DIR_NOTEBOOK = path.join(DIR_WORKDIR, 'notebook');
 
-[dir_workdir, dir_notebook].forEach(d => {
+[DIR_WORKDIR, DIR_NOTEBOOK].forEach(d => {
     if(!fs.existsSync(d)) fs.mkdirSync(d)
 });
 
+const FILTER_MD = (fp) => { return fp.match(/\.md$/); }
 
-function dummy() {
-    console.log('DUMMY');
-}
+const readFileRecursively = async(dir, filter, files=[]) => {
+    filter = filter || FILTER_MD;
 
-export { dummy };
+    const dirents = await fsPromises.readdir(dir, { withFileTypes: true });
+    const dirs = [];
+    for (const dirent of dirents) {
+        const fp = path.join(dir, dirent.name);
+        if(dirent.isDirectory()) dirs.push(fp);
+
+        if(!filter || filter(fp)) {
+            if(dirent.isFile()) files.push({
+                path: fp,
+                notebook: path.basename(dir),
+                file: path.basename(fp),
+            });
+        }
+    }
+    for (const d of dirs) {
+        files = await readFileRecursively(d, filter, files);
+    }
+    return Promise.resolve(files);
+};
+
+const readFile = fsPromises.readFile;
+
+export {
+    DIR_HOME, DIR_WORKDIR, DIR_NOTEBOOK,
+    readFileRecursively,
+    readFile,
+};
