cb1536d3efe385a6af45ec8300f23c1e8d2f4038
matsuura0831 <matsuura0831@gmail.com>
2020-10-10 15:01:53 +0900

    run: npm install @fortawesome/fontawesome-free

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/tailwind.css'
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
 
 createApp(App).mount('#app')
