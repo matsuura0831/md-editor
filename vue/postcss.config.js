ec74e4db62f381173c213f751963ae3a5db2fe57
matsuura0831 <matsuura0831@gmail.com>
2020-10-10 14:28:44 +0900

    use tailwindcss.

---
+++ vue/postcss.config.js
@@ -0,0 +1,15 @@
+const src = `${__dirname}/src`
+const dst = `${__dirname}/dist`
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+    content: [`${src}/**/*.html`, `${src}/**/*.vue`],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+    plugins: [
+        require("tailwindcss"),
+        require("autoprefixer"),
+        ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    ]
+};
