6e033d62180ebbbc80f7269b3d94083218e66e42
matsuura0831 <matsuura0831@gmail.com>
2020-10-11 21:45:54 +0900

    postcss setting.

--- vue/postcss.config.js
+++ vue/postcss.config.js
@@ -1,9 +1,30 @@
 const src = `${__dirname}/src`
-//const dst = `${__dirname}/dist`
 
 const purgecss = require("@fullhuman/postcss-purgecss")({
     content: [`${src}/**/*.html`, `${src}/**/*.vue`],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    /*
+    defaultExtractor: (content) => {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+    },
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    */
+    defaultExtractor: content => {
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+        return broadMatches.concat(innerMatches)
+    },
+    whitelistPatterns: [
+        /^vex.*/,
+        /^hljs.*/,
+        /^ace.*/,
+        /*
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/
+        */
+    ],
+
 });
 
 module.exports = {
